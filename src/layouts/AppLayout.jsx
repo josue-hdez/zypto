f7183f0886d50261fc7808ef017023dc607aca41
Josue Hernandez <josuehdez05232005@gmail.com>
2025-08-14 11:57:31 -0400

    Add AppLayout component

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -2,13 +2,25 @@ import { Outlet } from "react-router";
 import MarketStats from "../components/MarketStats";
 
 function AppLayout() {
+  const year = new Date().toLocaleDateString("en-US", { year: "numeric" });
+
   return (
     <>
       <header>
         <MarketStats />
+        <div className="border-bottom">
+          <div className="container">
+            <h1 className="text-xl lg:text-3xl">Zypto</h1>
+          </div>
+        </div>
       </header>
       <Outlet />
-      <footer></footer>
+      <footer className="border-bottom">
+        <div className="container">
+          <h2 className="text-xl">Zypto</h2>
+          <span>© {year} Zypto</span>
+        </div>
+      </footer>
     </>
   );
 }
